7ca684cd57eabb97c4c68171e4e22fa8c65089cc
zourtney <zourtney@gmail.com>
2014-04-28 23:46:53 -0500

    Fixing little runtime config problems

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,93 +30,93 @@ module.exports = function (grunt) {
       }
     },
 
-//     // Watches files for changes and runs tasks based on the changed files
-//     watch: {
-//       bower: {
-//         files: ['bower.json'],
-//         tasks: ['bowerInstall']
-//       },
-//       js: {
-//         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-//         tasks: ['newer:jshint:all'],
-//         options: {
-//           livereload: true
-//         }
-//       },
-//       jsTest: {
-//         files: ['test/spec/{,*/}*.js'],
-//         tasks: ['newer:jshint:test', 'karma']
-//       },
-//       compass: {
-//         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-//         tasks: ['compass:server', 'autoprefixer']
-//       },
-//       gruntfile: {
-//         files: ['Gruntfile.js']
-//       },
-//       livereload: {
-//         options: {
-//           livereload: '<%= connect.options.livereload %>'
-//         },
-//         files: [
-//           '<%= yeoman.app %>/{,*/}*.html',
-//           '.tmp/styles/{,*/}*.css',
-//           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-//         ]
-//       }
-//     },
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bowerInstall']
+      },
+      js: {
+        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: true
+        }
+      },
+      jsTest: {
+        files: ['test/spec/{,*/}*.js'],
+        tasks: ['newer:jshint:test', 'karma']
+      },
+      compass: {
+        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['compass:server', 'autoprefixer']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= yeoman.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
 
-//     // The actual grunt server settings
-//     connect: {
-//       options: {
-//         port: 9000,
-//         // Change this to '0.0.0.0' to access the server from outside.
-//         hostname: 'localhost',
-//         livereload: 35729
-//       },
-//       livereload: {
-//         options: {
-//           open: true,
-//           base: [
-//             '.tmp',
-//             '<%= yeoman.app %>'
-//           ]
-//         }
-//       },
-//       test: {
-//         options: {
-//           port: 9001,
-//           base: [
-//             '.tmp',
-//             'test',
-//             '<%= yeoman.app %>'
-//           ]
-//         }
-//       },
-//       dist: {
-//         options: {
-//           base: '<%= yeoman.dist %>'
-//         }
-//       }
-//     },
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            '<%= yeoman.app %>'
+          ]
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          base: [
+            '.tmp',
+            'test',
+            '<%= yeoman.app %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= yeoman.dist %>'
+        }
+      }
+    },
 
-//     // Make sure code styles are up to par and there are no obvious mistakes
-//     jshint: {
-//       options: {
-//         jshintrc: '.jshintrc',
-//         reporter: require('jshint-stylish')
-//       },
-//       all: [
-//         'Gruntfile.js',
-//         '<%= yeoman.app %>/scripts/{,*/}*.js'
-//       ],
-//       test: {
-//         options: {
-//           jshintrc: 'test/.jshintrc'
-//         },
-//         src: ['test/spec/{,*/}*.js']
-//       }
-//     },
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= yeoman.app %>/scripts/{,*/}*.js'
+      ],
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
 
     // Empties folders to start fresh
     clean: {
@@ -355,7 +355,8 @@ module.exports = function (grunt) {
         'compass'
       ],
       dist: [
-        'sass:dist', //'compass:dist',
+        'sass:dist',
+        // 'compass:dist',
         'imagemin',
         'svgmin'
       ]
@@ -397,33 +398,33 @@ module.exports = function (grunt) {
   });
 
 
-//   grunt.registerTask('serve', function (target) {
-//     if (target === 'dist') {
-//       return grunt.task.run(['build', 'connect:dist:keepalive']);
-//     }
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
 
-//     grunt.task.run([
-//       'clean:server',
-//       'bowerInstall',
-//       'concurrent:server',
-//       'autoprefixer',
-//       'connect:livereload',
-//       'watch'
-//     ]);
-//   });
+    grunt.task.run([
+      'clean:server',
+      'bowerInstall',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
 
-//   grunt.registerTask('server', function (target) {
-//     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-//     grunt.task.run(['serve:' + target]);
-//   });
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
 
-//   grunt.registerTask('test', [
-//     'clean:server',
-//     'concurrent:test',
-//     'autoprefixer',
-//     'connect:test',
-//     'karma'
-//   ]);
+  grunt.registerTask('test', [
+    'clean:server',
+    'concurrent:test',
+    'autoprefixer',
+    'connect:test',
+    'karma'
+  ]);
 
   grunt.registerTask('build', [
     'clean:dist',
--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// var http = require("http");
-
-// http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type": "text/plain"});
-//   response.write("Hello World");
-//   response.end();
-
-//   console.log("I am working");
-// }).listen(process.env.PORT || 8888);
-
 var express = require('express');
 var app = express();
 
