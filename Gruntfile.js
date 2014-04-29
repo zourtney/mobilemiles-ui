45ae80120ed021ba51d0932eb38f709f99ae5ae6
zourtney <zourtney@gmail.com>
2014-04-28 21:30:13 -0500

    load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@
 module.exports = function (grunt) {
 
 //   // Load grunt tasks automatically
-  // require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
 //   // Time how long tasks take. Can help when optimizing build times
 //   require('time-grunt')(grunt);
