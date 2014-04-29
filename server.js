d28ebe6464b558ec8c246282d0a7cb8865030476
zourtney <zourtney@gmail.com>
2014-04-28 23:29:33 -0500

    (Hopefully) legitimate app server
    Using express, just because

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var http = require("http");
+// var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
 
-  console.log("I am working");
-}).listen(process.env.PORT || 8888);
+//   console.log("I am working");
+// }).listen(process.env.PORT || 8888);
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 3000);
