55f7d1efbeb62677d937f2ce24b578b6ba2032d0
zourtney <zourtney@gmail.com>
2014-05-15 09:43:18 -0500

    Switched load order. (Moving scripts/app to coffeescript messed this up.)

--- karma.conf.js
+++ karma.conf.js
@@ -18,8 +18,8 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/scripts/*.js',
-      'app/scripts/**/*.js',
       '.tmp/scripts/*.js',
+      'app/scripts/**/*.js',
       '.tmp/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
