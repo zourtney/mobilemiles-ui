5ab12512c636ba376cbb01188f50363b1731dee4
zourtney <zourtney@gmail.com>
2014-07-17 18:52:53 -0500

    Hostname change: 'localhost' -> '127.0.0.1'.
    grunt-contrib-connect always opens 127.0.0.1 instead of localhost. Just keeping these in sync to prevent any cross-domain hiccups (despite the fact that 127.0.0.1 IS localhost)

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
 
   // Default deployment server
   var fallbackEnv = {
-    SERVER_URL: 'http://localhost:3000'
+    SERVER_URL: 'http://127.0.0.1:3000'
   }
 
   // Define the configuration for all the tasks
@@ -94,7 +94,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '127.0.0.1',
         livereload: 35729
       },
       livereload: {
